191d330f6c9d9e763526e5f980b185fb084463d7
wunschradio <39115843+wunschradio@users.noreply.github.com>
2026-09-06 13:32:46 +0200

    Update LocalWebhookHandler.php

--- backend/src/Webhook/LocalWebhookHandler.php
+++ backend/src/Webhook/LocalWebhookHandler.php
@@ -70,7 +70,6 @@ final class LocalWebhookHandler
         // Write JSON file to disk so nginx can serve it without calling the PHP stack at all.
         $this->logger->debug('Writing static nowplaying text file...');
 
-        $staticNpPath = $staticNpDir . '/' . $station->short_name . '.json';
         $fsUtils->dumpFile(
             $staticNpPath,
             json_encode($np, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) ?: ''
